df0d8654cc71f0be87d3237b2e412a601728eb17
TribQq <ghhghdffgj@gmail.com>
2022-02-02 17:02:09 +0500

    fix many-to-many link-bug

--- web-book_django/webBook/book0/models.py
+++ web-book_django/webBook/book0/models.py
@@ -23,9 +23,10 @@ class BookPage(models.Model):
 
 
 class PageLink(models.Model):
-    from_page = models.ForeignKey(BookPage, related_name='frommaterial', on_delete=models.CASCADE)  # ForeginKey - нативная ссылка для базы(1:33)0ур # foreginkey-всегда отношение многие к одному(многие стр к одной книге)
-    to_page = models.ForeignKey(BookPage, related_name='tomaterial', on_delete=models.CASCADE) # нужно было перереиминовать related_name чтоб он его не подтягивал из бука 2'02||0
+    from_page = models.ForeignKey(BookPage, on_delete=models.CASCADE)  # ForeginKey - нативная ссылка для базы(1:33)0ур # foreginkey-всегда отношение многие к одному(многие стр к одной книге)
+    to_page = models.ForeignKey(BookPage, related_name='to_page', on_delete=models.CASCADE) # нужно было перереиминовать related_name чтоб он его не подтягивал из бука 2'02||0
     name = models.TextField()
+    # проблема была в 2 related_name, нужно было как то указать, или удалить родительский
     def __str__(self):
         return ('{self.from_page.title} --> {self.to_page.title} '\
                '({self.id})'.format(
--- web-book_django/webBook/book0/views.py
+++ web-book_django/webBook/book0/views.py
@@ -23,7 +23,7 @@ def book_page(request, book_id, page_id):
 
 def page(request, book_id, page_id):
     return render(request, 'book0/page.html', context={
-    'page': get_object_or_404(BookPage, book__id=book_id, id=page_id, ), # # __ = ppep идём в соседнюю табл
+    'page': get_object_or_404(BookPage, book__id=book_id, id=page_id ) # # __ = ppep идём в соседнюю табл
     })
 
 
