147364328941db75b1c215445be2af03c3ab7ead
TribQq <ghhghdffgj@gmail.com>
2022-02-13 22:15:42 +0500

    hide_urls_for_users +mini_optimize_views

--- web-book_django/webBook/book0/urls.py
+++ web-book_django/webBook/book0/urls.py
@@ -5,8 +5,8 @@ from . import views
 urlpatterns = [
     path('', views.index),
     path('book/<int:book_id>/', views.book, name='book'),
-    path('book/<int:book_id>/page/<int:page_id>', views.page, name='page'),
-    path('book/<int:book_id>/page/<int:page_id>/take/<int:item_id>/', views.take, name='take'),
+    path('book/<int:book_id>/go/<int:pagelink_id>', views.go_to, name='go_to'),
+    path('book/<int:book_id>/take/<int:item_id>/', views.take, name='take'),
     path('book/<int:book_id>/map.svg', views.view_book_map, name='view_book_map'),
     path('plug', views.plug, name='plug'),
 ]
--- web-book_django/webBook/book0/views.py
+++ web-book_django/webBook/book0/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse, FileResponse
+from django.urls import reverse
+
 
 from graphviz import Digraph
 
 from .models import *
 
-from django.urls import reverse
+
+def _return_to(book_id):
+    return redirect(reverse('book', kwargs={'book_id': book_id}))
 
 
 def on_progress(view):
@@ -14,7 +18,8 @@ def on_progress(view):
             progress = BookProgress.objects.get(book=book_id, user=request.user)
         except BookProgress.DoesNotExist:
             return redirect(reverse('book', kwargs={'book_id': book_id}))
-        return view(request=request, progress=progress, book_id=book_id, **kwargs) # генерируем прогресс для каждой функции с помощью декоратоа
+        return view(request=request, progress=progress, book_id=book_id,
+                    **kwargs)  # генерируем прогресс для каждой функции с помощью декоратоа
 
     return inner
 
@@ -23,47 +28,52 @@ def index(request):
     return render(request, 'book0/bookshelf.html', context={'books': Book.objects.all()})
 
 
-@on_progress
-def book(request, progress, book_id):
-    # b = get_object_or_404(Book, id=book_id)
-    # if not b.first_page:
-    #     return render(request, 'book0/book.html', context={
-    #         'book': get_object_or_404(Book, id=book_id)
-    #     })
-    # try:
-    #     progress = BookProgress.objects.get(book=b, user=request.user)
-    # except BookProgress.DoesNotExist:
-    #     progress = BookProgress.start_reading(user=request.user, book=b)
-    return redirect(reverse('page', kwargs={'book_id': book_id, 'page_id': progress.book_page.id, }))
-    # return redirect(reverse(plug)) # в плаг нельзя передавать агрументы .С url траблы (копнуть глубже) return redirect(reverse('plug', kwargs={'text': 'randomText'}))
+# @on_progress
+def book(request, book_id: int) -> render:
+    book = get_object_or_404(Book, id=book_id)
+    assert book.first_page, 'cant_get_book_Error'
+    try:
+        progress = BookProgress.objects.get(book=book, user=request.user)
+    except BookProgress.DoesNotExist:
+        progress = BookProgress.start_reading(user=request.user, book=book)
 
+    page = progress.book_page
 
-@on_progress
-def page(request, progress, book_id, page_id):
-    page = get_object_or_404(BookPage, book__id=book_id, id=page_id)
-    progress.book_page = page
-    progress.save()
-
-    links = [
-        (
-            link, link.check_items(list(progress.items.all()) # (link , status_link)
-                                   )
-        )
+    links: list[tuple[any, bool], ...] = [
+        (link, link.check_items(list(progress.items.all())))
         for link in page.pagelink_set.all()
     ]
-    return render(request, 'book0/page.html', context={
-        'page': page,  # # __ = ppep идём в соседнюю табл
-        'progress': progress,
-        'link_status_tuples': links,
-        'page_items': page.items.exclude(id__in=progress.items.only('id'))
-    })
+    return render(request, 'book0/page.html',
+                  context={
+                      'page': page,  # # __ = ppep идём в соседнюю табл
+                      'progress': progress,
+                      'link_status_tuples': links,
+                      'page_items': page.items.exclude(id__in=progress.items.only('id'))
+                  })
 
 
 @on_progress
-def take(request, progress, book_id, page_id, item_id):
+def go_to(request, progress, book_id: int, pagelink_id: int):
+    link = get_object_or_404(PageLink, id=pagelink_id)
+    if (
+        link.from_page.id == progress.book_page.id # no upd problem idk ( (fixed) fail id luil)
+        and
+        link.check_items(progress.items.all())
+    ):
+        progress.book_page = link.to_page # если проходит if , то ресэйвим прогресс иначе не проходит оновление прогресса и -> не ченджится страница
+        progress.save()
+    return _return_to(book_id=book_id)
+
+
+@on_progress
+def take(request, progress, book_id, item_id):
     item = get_object_or_404(Item, id=item_id)
-    progress.items.add(item)
-    return redirect(reverse('page', kwargs={'book_id': book_id, 'page_id': page_id, }))
+    if (
+        item in progress.book_page.items.all()
+    ):
+        progress.items.add(item)
+    return _return_to(book_id=book_id)
+
 
 
 def plug(request, text='text'):
@@ -91,15 +101,15 @@ def view_book_map(request, book_id):
         )
 
     for link in PageLink.objects.filter(
-        from_page__book_id=book_id,
+            from_page__book_id=book_id,
     ).all():
         g.edge(pid(link.from_page), pid(link.to_page), label='\n'.join(
             [str(link.id), link.name[:10]] + [
                 i.name for i in link.items.all()
             ]),
-            labeltooltip=link.name,
-            labelhref='/admin/book0/pagelink/{}/change'.format(link.id),
-        )
+               labeltooltip=link.name,
+               labelhref='/admin/book0/pagelink/{}/change'.format(link.id),
+               )
 
     g.render(quiet=True, view=False, format='svg')
     return FileResponse(open('/tmp/map.gv.svg', 'rb'))
