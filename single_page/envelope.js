ad2017025aaeb273c47249bef461423b6699cf63
TribQq <ghhghdffgj@gmail.com>
2022-03-16 19:47:32 +0500

    remove_single_page

--- single_page/envelope.js
+++
@@ -1,9 +0,0 @@
-{
-  document.querySelector(".envelope").addEventListener("click", function () {
-   this.classList.toggle("fold");
- });
- 
- setTimeout(function () {
-   document.querySelector(".envelope").classList.add("fold");
- }, 2000);
- }
