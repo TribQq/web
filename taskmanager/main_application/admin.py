ce435e3788479a1e4c75bae059fad2d705c246d6
TribQq <ghhghdffgj@gmail.com>
2022-03-24 15:35:59 +0500

    clean up  up repo

--- taskmanager/main_application/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Tabli4ka
-
-admin.site.register(Tabli4ka)
-
-
-# Register your models here.
--- web-book_django/webBook/book0/utilities.py
+++
@@ -1,6 +0,0 @@
-from datetime import datetime
-from os.path import splitext
-
-
-def get_timestamp_path(instance, filename, used_in = 'cover_img_'):
-    return '%s%s%s' %(used_in, datetime.now().timestamp(), splitext(filename)[1])
